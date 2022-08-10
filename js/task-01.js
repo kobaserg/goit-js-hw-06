9e2e1c850eb01857a0fb839f53010cd38040e9a0
kobaserg <kobasts@gmail.com>
2022-08-10 11:43:14 +0200

    ДЗ6 продолжение

--- js/task-01.js
+++ js/task-01.js
@@ -2,9 +2,6 @@
 const numberOfCat = document.querySelectorAll('li.item');
 console.log(`Number of categories:`, numberOfCat.length);
 
-
-// console.log(numberOfCat);
-
 const itemCat = numberOfCat.forEach(el => {
     const elCat = el.querySelector('h2').innerText; 
     console.log(`Category:`, elCat);
@@ -12,6 +9,3 @@ const itemCat = numberOfCat.forEach(el => {
     console.log(`Elements`, numberItemCat.length);
 
 });
-// const itemCatTitle = itemCat.innerText
-// console.log(itemCat)
-// console.log(itemCatTitle)
