0bc8b38c67e6544d684d28b7b576940462d2474c
kobaserg <kobasts@gmail.com>
2022-08-16 16:30:57 +0200

    ДЗ6 Задание 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0).padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const colorName = document.querySelector('span.color')
+
+const buttonColor = document.querySelector('button.change-color');
+
+buttonColor.addEventListener('click', (event) => {
+  let currentColorBody = getRandomHexColor();
+  bodyEl.classList.add('.color-body');
+  bodyEl.style.backgroundColor = currentColorBody;
+  colorName.textContent = currentColorBody;
+  colorName.style.fontSize = '36px'
+  colorName.style.color = 'white'
+})
