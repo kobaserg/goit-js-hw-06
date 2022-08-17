2f0d9ed7ae3d019e46178e72b43846ae4863af71
kobaserg <kobasts@gmail.com>
2022-08-17 14:19:14 +0200

    ДЗ6 доработка

--- js/task-06.js
+++ js/task-06.js
@@ -1,21 +1,15 @@
 const inputEl = document.querySelector('#validation-input');
 
-inputEl.addEventListener('focus', inputFocus);
 inputEl.addEventListener('blur', inputBlur);
 
-let countLetter = 0;
-
-function inputFocus() {
-    const inputString = addEventListener('input', event => {
-        countLetter += 1;
-})  
-}
-
-function inputBlur() {
-    if (countLetter === Number(inputEl.dataset.length)) {  
-        inputEl.classList.toggle('valid');
-    } 
-    else {
-        inputEl.classList.toggle('invalid');
-    }
+function inputBlur(symbol) {
+  const input = symbol.currentTarget;
+  const inputLength = Number(input.dataset.length);
+  if (input.value.length === inputLength) {
+    input.classList.remove('invalid');
+    input.classList.add('valid');
+  } else {
+    input.classList.remove('valid');
+    input.classList.add('invalid');
+  }
 }
