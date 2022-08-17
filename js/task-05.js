a3becd64065447dd70791d96f9ee002e8587e7dd
kobaserg <kobasts@gmail.com>
2022-08-17 16:35:14 +0200

    ДЗ6 Задание 5 shift

--- js/task-05.js
+++ js/task-05.js
@@ -1,14 +1,13 @@
 const input = document.querySelector('#name-input');
 const output = document.querySelector('#name-output');
 
-let keyPressCounter = 1;
+let keyPressCounter = 0;
 
-const inputName = addEventListener('keydown', event => {
-    if (keyPressCounter === 1) {
-        output.textContent = '';
-    }  
-    output.textContent +=  event.key;
-    keyPressCounter += 1;
+input.addEventListener('keydown', event => {
+  keyPressCounter += 1;
+  if (keyPressCounter === 1) output.textContent = '';
+  if (event.key !== 'Shift') {
+    output.textContent += event.key;
+  }
+  console.log(event.key);
 });
-
-
